df946b57aa5fb51ffe5d1be00be04e1d44686e49
EC2 Default User <ec2-user@ip-172-31-81-76.ec2.internal>
2021-07-19 04:36:32 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
-  root to: 'homes#top'
+  resources :homes
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
